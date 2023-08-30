87450272c5884dcb791bce515bbe87078915ed59
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-08-30 19:02:46 +0800

    feat: ICpEErs registration

--- src/pages/BeOurPeer.jsx
+++ src/pages/BeOurPeer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import Container from '../components/Container'
+import React from 'react';
+import Container from '../components/Container';
+import { icpeers_qr } from '../utils/data/icpep';
 
 const BeOurPeer = () => {
-  return (
-    <Container>BeOurPeer</Container>
-  )
-}
+	return (
+		<div className="bg-pale w-full py-14 lg:py-16">
+			<div className="lg:flex justify-center items-center gap-x-4 text-dark px-2">
+				<div className="flex justify-center py-5">
+					<img
+						className="bg-dark border-2 border- rounded"
+						width={230}
+						src={`${icpeers_qr}`}
+						alt="icpeers-registration"
+					/>
+				</div>
 
-export default BeOurPeer
+				<div className="flex flex-col gap-y-1 lg:gap-y-4">
+					<h1 className="font-bricksans text-xl lg:text-3xl text-center lg:text-left">
+						Be our Peer!
+					</h1>
+					<p className="font-garet text-center lg:text-left">
+						Sign up for iCPEers Volunteers Edition Now!
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default BeOurPeer;
