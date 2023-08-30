de54f236252c3f01b6b5868d461feee4d5f1cf0a
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-08-30 18:15:51 +0800

    refactor: added carousel for tips

---
+++ src/components/TipsSlider.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import TipCard from './TipCard';
+import { seniorTips } from '../utils/data/tips';
+
+const TipsSlider = () => {
+	return (
+		<Carousel
+			autoPlay
+			swipeable
+			infiniteLoop
+			interval={4000}
+			showIndicators={false}
+			transitionTime={1000}
+		>
+			{seniorTips.map((tip) => (
+				<div>
+					<TipCard
+						dark={seniorTips.indexOf(tip) % 2 == 0 ? false : true}
+						message={tip.message}
+						senior_name={tip.name}
+						senior_age={tip.age}
+					/>
+				</div>
+			))}
+		</Carousel>
+	);
+};
+
+export default TipsSlider;
