31c4b06e5c123ce6be693f184b7b9be362bf3a5e
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-08-31 11:26:57 +0800

    feat: added aos animation

--- src/pages/PressStart.jsx
+++ src/pages/PressStart.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import { gradientBg } from '../utils/gradientbg'
+import React, { useEffect } from 'react';
+// import Aos from 'aos';
+import { gradientBg } from '../utils/gradientbg';
 import { icpep, wow } from '../utils/data/icpep';
 
 const PressStart = () => {
-    const _gradientBg = gradientBg();
+	const _gradientBg = gradientBg();
 
-    return (
-        <div className={`static flex justify-center text-white items-center min-h-screen w-full ${_gradientBg} bg-nebula bg-center bg-cover `}>
-            <div className="absolute top-0 lg:top-0 lg:right-0 p-16">
-                <img width={150} src={`${icpep.logo}`} alt="" />
-            </div>
-            <div className="flex justify-center items-center ">
-                <div className="flex flex-col">
-                    <img width={1500} src={`${wow.press_start}`} alt="press-start" />
-                    <h1 className="text-center text-xl md:text-4xl lg:text-6xl uppercase font-arcade">
-                        A Week of Welcome Edition
-                    </h1>
-                </div>            
-            </div>
-        </div>
-    )
-}
+	return (
+		<div
+			data-aos="fade-in"
+			data-aos-duration="1000"
+			data-aos-delay="50"
+			className={`static flex justify-center text-white items-center min-h-screen w-full ${_gradientBg} bg-nebula bg-center bg-cover `}
+		>
+			<div className="absolute top-0 lg:top-0 lg:right-0 p-16">
+				<img width={150} src={`${icpep.logo}`} alt="" />
+			</div>
+			<div className="flex justify-center items-center ">
+				<div className="flex flex-col">
+					<img width={1500} src={`${wow.press_start}`} alt="press-start" />
+					<h1 className="text-center text-xl md:text-4xl lg:text-6xl uppercase font-arcade">
+						A Week of Welcome Edition
+					</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default PressStart
+export default PressStart;
